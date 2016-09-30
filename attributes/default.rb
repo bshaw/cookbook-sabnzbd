fcf0a3c763b7c61b541e4a0507b0b4bdbf2f1a94
Brian Shaw <bshaw@virtusa.com>
2016-09-30 21:11:10 +0100

    Configure sabnzbd container

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+default['sabnzbd']['docker_image'] = 'timhaak/sabnzbd'
+default['sabnzbd']['image_tag'] = 'latest'
+default['sabnzbd']['host_port'] = 8080
+default['sabnzbd']['container_port'] = 8080
+default['sabnzbd']['host_name'] = 'sabnzbd'
+default['sabnzbd']['domain_name'] = 'example.com'
+default['sabnzbd']['config_volume'] = '/sabnzbd/config'
+default['sabnzbd']['data_volume'] = '/sabnzbd/data'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,23 @@
 # Cookbook Name:: sabnzbd
 # Recipe:: default
 #
+
+# get the image from dockerhub
+docker_image node['sabnzbd']['docker_image']
+
+# run the container
+docker_container 'sabnzbd' do
+  repo node['sabnzbd']['docker_image']
+  tag node['sabnzbd']['image_tag']
+  port "#{node['sabnzbd']['host_port']}:#{node['sabnzbd']['container_port']}"
+  restart_policy 'always'
+  host_name node['sabnzbd']['host_name']
+  domain_name node['sabnzbd']['domain_name']
+  volumes ["#{node['sabnzbd']['config_volume']}:/config", "#{node['sabnzbd']['data_volume']}:/data"]
+end
+
+# open port in firewall
+firewall_rule 'sabnzbd' do
+  port node['sabnzbd']['host_port']
+  command :allow
+end
